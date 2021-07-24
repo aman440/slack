c7194c317001f8d0c6a673b75a1b380a077c0470
Aman Pratap Singh <arunkumar9101163177@gmail.com>
2021-07-25 00:30:49 +0530

    connected to firebase

--- slack-clone/src/firebase.js
+++ slack-clone/src/firebase.js
@@ -1,4 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// if
+//           request.time < timestamp.date(2021, 8, 23);
+
+import firebase from 'firebase';
+// import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyDaz8PaSyDjRG8-2de3I6zHrWzcMIiY8e0",
     authDomain: "slack-bcf0f.firebaseapp.com",
@@ -8,3 +14,12 @@ const firebaseConfig = {
     appId: "1:261820938860:web:8619846547ceaeaee6b649",
     measurementId: "G-8C27P2EX6P"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export {auth, provider, db};
+
+  
